33e9ecda7968c08d0c20a32b958dfdcbb7cfd712
haram082 <haramkim082@gmail.com>
2025-02-25 16:49:51 -0800

    final b4 deploy

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -238,16 +238,30 @@ const Header = () => {
                                         onClick={() => window.location.href = `${process.env.BACKEND_LINK}/api/auth/logout/saml`}
                                         className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
                                     >
-                                        Logout
+                                        Logout SSO
                                     </button>
+                                    {/* <button
+                                        onClick={logout}
+                                        className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+                                    >
+                                        Logout
+                                    </button> */}
                                 </>
                             ) : (
+                                <>
                                 <button
                                     onClick={() => window.location.href = `${process.env.BACKEND_LINK}/api/auth/login/saml`}
                                     className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
                                 >
-                                    Login
+                                    Login SSO
                                 </button>
+                                {/* <button
+                                    onClick={() => router.push('/login')}
+                                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                                >
+                                    Login
+                                </button> */}
+                                </>
                             )}
                         </nav>
 
