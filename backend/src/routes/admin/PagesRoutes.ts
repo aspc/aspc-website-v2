b0cf3ec0a0a232639a490e2ffa762be714202091
Vadym Musiienko <musiienkovadym@gmail.com>
2025-02-27 16:44:00 -0800

    Create admin folder and move StaffRoutes and PagesRoutes (formerly AdminRoutes) there

--- backend/src/routes/admin/PagesRoutes.ts
+++ backend/src/routes/admin/PagesRoutes.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import PageContent from "../models/PageContent";
+import PageContent from "../../models/PageContent";
 
 const router = express.Router();
 
