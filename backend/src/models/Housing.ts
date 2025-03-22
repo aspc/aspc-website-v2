49a7a278e26f9e6f3f695d5e9b3e99ff63c208d5
ksantoso2 <130810746+ksantoso2@users.noreply.github.com>
2025-03-22 16:14:40 -0700

    comment out housing suite id for now

--- backend/src/models/Housing.ts
+++ backend/src/models/Housing.ts
@@ -44,7 +44,7 @@ interface IHousingRooms extends Document {
     occupancy_type?: number;
     closet_type?: number;
     bathroom_type?: number;
-    housing_suite_id?: number; // DELETE
+    // housing_suite_id?: number; // DELETE
     housing_building_id: number;
     room_number: string;
 }
@@ -67,11 +67,11 @@ const HousingRoomsSchema = new Schema<IHousingRooms>({
     bathroom_type: {
         type: Number,
     },
-    housing_suite_id: { // DELETE
-        type: Number, 
-        ref: 'HousingSuites',
-        index: true
-    },
+    // housing_suite_id: { // DELETE
+    //     type: Number, 
+    //     ref: 'HousingSuites',
+    //     index: true
+    // },
     housing_building_id: {
         type: Number,
         required: true,
