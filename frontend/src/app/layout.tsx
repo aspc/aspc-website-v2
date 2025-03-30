a19fa9054ed5f489f30000838ff383d456a52020
haram082 <haramkim082@gmail.com>
2025-03-30 13:37:21 -0700

    emergerncy maintance shutdown?!?!?

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/ui/Header'
 import Footer from '@/components/ui/Footer'
 import './globals.css'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Main } from 'next/document'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,15 @@ export const metadata = {
   },
 
 };
+
+function MaintenancePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 text-center">
+      <h1 className="text-2xl font-bold mb-4">Website Temporarily Unavailable</h1>
+      <p>We're performing maintenance. Please check back later.</p>
+    </div>
+  )
+}
 
 export default function RootLayout({
   children,
@@ -26,7 +36,9 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Header />
-        {children}
+        {/* {children} */}
+        <MaintenancePage/>
+
         <SpeedInsights />
         <Footer />
       </body>
