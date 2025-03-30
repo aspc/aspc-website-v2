03e9d56a8d78a29518d8ee26e789fb3f62b01e1f
haram082 <haramkim082@gmail.com>
2025-03-30 13:41:30 -0700

    maintance

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import Header from '@/components/ui/Header'
 import Footer from '@/components/ui/Footer'
 import './globals.css'
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Main } from 'next/document'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,14 +11,13 @@ export const metadata = {
   icons: {
     icon: "/logo-square.png"
   },
-
 };
 
 function MaintenancePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 text-center">
       <h1 className="text-2xl font-bold mb-4">Website Temporarily Unavailable</h1>
-      <p>We're performing maintenance. Please check back later.</p>
+      <p>We&apos;re currently facing unexpected issues. Please check back later.</p>
     </div>
   )
 }
@@ -29,18 +27,25 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  // Check for maintenance mode environment variable
+  const isMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true'
+
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/logo-square.png" type="image/png" />
       </head>
       <body className={inter.className}>
-        <Header />
-        {/* {children} */}
-        <MaintenancePage/>
-
-        <SpeedInsights />
-        <Footer />
+        {isMaintenanceMode ? (
+          <MaintenancePage />
+        ) : (
+          <>
+            <Header />
+            {children}
+            <SpeedInsights />
+            <Footer />
+          </>
+        )}
       </body>
     </html>
   )
