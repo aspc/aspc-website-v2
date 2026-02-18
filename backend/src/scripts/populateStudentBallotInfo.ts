b0324256ef4adba711445d6bb5249a345821104c
Haram Yoon <haramkim082@gmail.com>
2026-02-18 01:04:35 -0800

    scirpt

--- backend/src/scripts/populateStudentBallotInfo.ts
+++ backend/src/scripts/populateStudentBallotInfo.ts
@@ -3,16 +3,10 @@ import { StudentBallotInfo } from '../models/Voting';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const ELECTION_ID = '698ba8595d9257bf8fb8be8d';
-const EMAILS: string[] = [
-    'ljbq2023@mymail.pomona.edu',
-    'gzfs2022@mymail.pomona.edu',
-    'smfk2024@mymail.pomona.edu',
-];
+const ELECTION_ID = '';
+const EMAILS: string[] = [];
 
-const MONGO_URI =
-    process.env.MONGODB_TEST_URI ||
-    'mongodb+srv://software:jxqDmaRabKr7BXuS@aspc.qm4l8.mongodb.net/school-platform-test?retryWrites=true&w=majority&appName=ASPC';
+const MONGO_URI = process.env.MONGODB_TEST_URI || '';
 async function populateStudentBallotInfo() {
     if (!ELECTION_ID) throw new Error('ELECTION_ID is required');
     if (EMAILS.length === 0) throw new Error('EMAILS array is empty');
