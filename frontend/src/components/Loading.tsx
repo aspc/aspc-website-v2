f2a8f8878a9378c0f3e0c57eb21554c6781f8733
Haram Yoon <haramkim082@gmail.com>
2026-05-02 20:29:46 -0700

    Merge branch 'main' of https://github.com/aspc/aspc-website-v2 into feat/cecil-loading-screen

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             <video
-                src="/cecil-run.mp4"
+                src="/cecil_loading.mp4"
                 autoPlay
                 loop
                 muted
